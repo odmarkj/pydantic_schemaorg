5921d32d154b1e346c641dd7ae94c295cff2a366
Joshua Odmark <joshuaeric@gmail.com>
2025-08-29 12:13:50 -0700

    Update ISO8601Date.py

--- pydantic_schemaorg/ISO8601/ISO8601Date.py
+++ pydantic_schemaorg/ISO8601/ISO8601Date.py
@@ -2,9 +2,9 @@ import re
 from typing import no_type_check, Optional, Dict, cast, Any, Pattern, TYPE_CHECKING, Generator, AnyStr, Union
 
 from pydantic import BaseConfig
-from pydantic.fields import ModelField
+from pydantic.v1.fields import ModelField
 from pydantic.utils import update_not_none
-from pydantic.validators import str_validator, constr_length_validator
+from pydantic.v1.validators import str_validator, constr_length_validator
 
 from pydantic_schemaorg.ISO8601 import errors
 
